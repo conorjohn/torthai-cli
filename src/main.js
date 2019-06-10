bce95b816a83bd34ad848257f7ce69d7ab5b640b
conorjohn <conorj92@gmail.com>
2019-06-10 22:10:03 +0100

    chore: consumable as a module, publishable to npm

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import fs from 'fs';
 import ncp from 'ncp';
 import path from 'path';
 import { promisify } from 'util';
+import execa from 'execa';
+import Listr from 'listr';
+import { projectInstall } from 'pkg-install';
 
 const access = promisify(fs.access);
 const copy = promisify(ncp);
@@ -11,6 +14,16 @@ async function copyTemplateFiles(options) {
     return copy(options.templateDirectory, options.targetDirectory, {
         clobber: false,
     });
+}
+
+async function initGit(options) {
+    const result = await execa('git', ['init'], {
+        cwd: options.targetDirectory,
+    });
+    if (result.failed) {
+        return Promise.reject(new Error('Failed to initialize git'));
+    }
+    return;
 }
 
 export async function createProject(options) {
@@ -25,7 +38,7 @@ export async function createProject(options) {
         '../../templates',
         options.template.toLowerCase()
     );
-    
+
     let regex = /%20/g;
     let dir = templateDir.replace(regex, ' ').substring(3);
     options.templateDirectory = dir;
@@ -38,12 +51,30 @@ export async function createProject(options) {
     }
 
     console.log('Copy project files');
-    try {
-        await copyTemplateFiles(options);
-    } catch (err) {
-        console.log('Error: ', err);
-    }
+    const tasks = new Listr([
+        {
+            title: 'Copy project files',
+            task: () => copyTemplateFiles(options),
+        },
+        {
+            title: 'Initialize git',
+            task: () => initGit(options),
+            enabled: () => options.git,
+        },
+        {
+            title: 'Install dependencies',
+            task: () =>
+                projectInstall({
+                    cwd: options.targetDirectory,
+                }),
+            skip: () =>
+                !options.runInstall
+                    ? 'Pass --install to automatically install dependencies'
+                    : undefined,
+        },
+    ]);
 
+    await tasks.run();
     console.log('%s Project ready', chalk.green.bold('DONE'));
     return true;
 }
